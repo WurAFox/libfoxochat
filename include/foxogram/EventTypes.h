861876d6780c590da5cab204275cabca81e88445
wurafox <wurafoxxi@gmail.com>
2025-01-11 15:53:21 +0200

    Add events and event types

---
+++ include/foxogram/EventTypes.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <foxogram/export.h>
+#include <foxogram/Message.h>
+#include <foxogram/Channel.h>
+#include <foxogram/Member.h>
+#include <foxogram/User.h>
+#include <foxogram/Me.h>
+
+namespace foxogram {
+    struct LIBFOXOGRAM_EXPORT Event_t {
+            public:
+            std::string rawEvent = {};
+            Me* me = nullptr;
+            Event_t() = default;
+            Event_t(const Event_t& e) = default;
+            Event_t(Event_t&& e) = default;
+            Event_t(Me* me, const std::string& raw);
+            Event_t(Me* me, std::string&& raw);
+            Event_t& operator=(const Event_t& e);
+            Event_t& operator=(Event_t&& e);
+            virtual ~Event_t() = 0;
+    };
+
+    struct LIBFOXOGRAM_EXPORT MessageCreate_t : public Event_t {
+        using Event_t::Event_t;
+        using Event_t::operator=;
+        Message* msg = nullptr;
+        void send(const std::string& m) const;
+    };
+
+    struct LIBFOXOGRAM_EXPORT MessageUpdate_t : public Event_t {
+        using Event_t::Event_t;
+        using Event_t::operator=;
+        Message* msg = nullptr;
+    };
+
+    struct LIBFOXOGRAM_EXPORT MessageDelete_t : public Event_t {
+        using Event_t::Event_t;
+        using Event_t::operator=;
+        Message* msg = nullptr;
+    };
+
+    struct LIBFOXOGRAM_EXPORT ChannelUpdate : public Event_t {
+        using Event_t::Event_t;
+        using Event_t::operator=;
+        Channel* channel = nullptr;
+    };
+
+    struct LIBFOXOGRAM_EXPORT ChannelDelete_t : public Event_t {
+        using Event_t::Event_t;
+        using Event_t::operator=;
+        Channel* channel = nullptr;
+    };
+
+    struct LIBFOXOGRAM_EXPORT MemberAdd_t : public Event_t {
+        using Event_t::Event_t;
+        using Event_t::operator=;
+        Member* member = nullptr;
+    };
+
+    struct LIBFOXOGRAM_EXPORT MemberRemove_t : public Event_t {
+        using Event_t::Event_t;
+        using Event_t::operator=;
+        Member* member = nullptr;
+    };
+
+    struct LIBFOXOGRAM_EXPORT MemberUpdate_t : public Event_t {
+
+    };
+}
---
+++ include/foxogram/Events.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <foxogram/export.h>
+#include <nlohmann/json.hpp>
+
+#define event_decl(x) \
+	class x : public Event { public: virtual void handle(Me* me, nlohmann::json j, const std::string raw); };
+
+namespace foxogram {
+    class Me;
+    namespace events {
+        class LIBFOXOGRAM_EXPORT Event {
+            virtual void handle(Me *me, nlohmann::json j, std::string raw) = 0;
+        };
+
+        event_decl(MessageCreate)
+        event_decl(MessageUpdate)
+        event_decl(MessageDelete)
+//        event_decl(ChannelCreate)
+        event_decl(CHANNEL_UPDATE)
+        event_decl(ChannelDelete)
+        event_decl(MemberAdd)
+        event_decl(MemberRemove)
+        event_decl(MemberUpdate)
+        event_decl(Hello)
+        event_decl(Pong)
+
+    }
+}
